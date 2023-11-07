2b2752250089d15a92d82112d6cb289e6fbb3f4a
Hiba-Mohamed <127333097+Hiba-Mohamed@users.noreply.github.com>
2023-11-06 22:13:00 -0330

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{html,js, tsx, ts}",
+    "./components/**/*.{html,js, tsx, ts}",
+  ],
   theme: {
     extend: {},
   },
